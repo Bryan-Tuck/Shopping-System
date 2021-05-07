7d1bb9a57c7993cc01500a18704d0bac808563f5
Bryan Tuck <BryanTuck@ymail.com>
2021-05-07 01:01:51 -0500

    Updated Customer Class

--- ShoppingSystem/Customer.py
+++ ShoppingSystem/Customer.py
@@ -8,6 +8,9 @@ Created on Thu May  6 16:40:08 2021
 
 
 class Customer:
+    
+    cusCount = 0
+    
     def __init__(self, first_name, last_name, street, city, zip_address):
         self.first_name = first_name
         self.last_name = last_name
@@ -20,17 +23,29 @@ class Customer:
     def fullname(self):
         return "{} {}".format(self.first_name, self.last_name)
     
+    def street(self, street):
+        return street
+    
+    def city(self, city):
+        return city
+    
+    def zip_address(self, zip_address):
+        return zip_address
+        
     def add_customer(self, Customer):
         self.customer_data.append(Customer)
         
+    def display_Customer(self):
+        print ("Name:", self.fullname(), "Street:", self.street,
+        "City:", self.city, "Zip Address:", self.zip_address)
+                
         
                                  
            
-cust_1 = Customer('Steve', 'Jobs', '10912 Golden Drive', 'Houston', 77044)
-customer.add_customer(Customer)
+customer_1 = Customer('Steve', 'Jobs', '10912 Golden Drive', 'Houston', 77044)
 
+customer_1.display_Customer()
 
-print(cust_1.fullname())
 
 
 
